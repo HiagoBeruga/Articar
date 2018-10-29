48e56c6e1b14dd17c62a837545f07206c9ba702c
HiagoBeruga <hiagomess@outlook.com>
2018-10-29 20:54:39 -0300

    acho q e isso memo

---
+++ myApp/src/pages/velocimetro/velocimetro.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { IonicPageModule } from 'ionic-angular';
+import { VelocimetroPage } from './velocimetro';
+
+@NgModule({
+  declarations: [
+    VelocimetroPage,
+  ],
+  imports: [
+    IonicPageModule.forChild(VelocimetroPage),
+  ],
+})
+export class VelocimetroPageModule {}
