65edeb317fd629427416b0d0527f8780636cae36
HiagoBeruga <hiagomess@outlook.com>
2018-11-26 15:27:17 -0200

    agora tem barra de atualização na aba velocímetro

--- myApp/src/pages/velocimetro/velocimetro.ts
+++ myApp/src/pages/velocimetro/velocimetro.ts
@@ -15,6 +15,14 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class VelocimetroPage {
 
+  doRefresh(refresher) {
+    console.log('Begin async operation', refresher);
+
+    setTimeout(() => {
+      console.log('Async operation has ended');
+      refresher.complete();
+    }, 2000);
+  }
   constructor(public navCtrl: NavController, public navParams: NavParams) {
   }
 
